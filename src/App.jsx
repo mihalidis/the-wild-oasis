971372378bb9818e948d4cc1cd87f7713c47d1e8
pinarsuvacoglu <pinarsuvac@gmail.com>
2026-01-29 16:37:05 +0300

    add styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import GlobalStyles from "./styles/GlobalStyles.js";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
